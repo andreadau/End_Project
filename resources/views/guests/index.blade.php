5ab726fac35f3db8c2faac34535c34869be0fe66
Angela Caterina Gallo <angela.caterina@icloud.com>
2021-03-15 13:20:16 +0100

    struttura cartelle e modifica login

---
+++ resources/views/guests/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.guests.app')
+
+@section('content')
+<div class="content">
+    <div class="title m-b-md">
+        End_Project
+    </div>
+
+    <div class="links">
+        <a href="https://github.com/andreadau/End_Project">Github</a>
+    </div>
+</div>
+@endsection
