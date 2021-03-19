3afa1430f6a5a34bc2b165afeb618cc283bd1051
andreadau <dauriaandrea99@gmail.com>
2021-03-19 17:47:32 +0100

    Search Component API_ADD

--- resources/js/views/SearchComponent.vue
+++ resources/js/views/SearchComponent.vue
@@ -1,12 +1,14 @@
 <template>
 
     <div>
-        <div>
-                <navbarsearch-component></navbarsearch-component>
-            <div>
-
+            <navbarsearch-component></navbarsearch-component>
+            <div v-for="(type,index) in types" >
+                <span>{{type.name}}</span>
             </div>
-        </div>
+            <!-- <div v-for="(restaurant,index) in restaurants">
+                <p>{{restaurant.name}}</p>
+                <p>{{restaurant.address}}</p>
+            </div> -->
     </div>
 
 </template>
@@ -15,18 +17,27 @@
     export default {
         data(){
             return {
-                restaurants: { type: Object, default: () => ({}) }
+                types: "",
+                restaurants: ""
             } 
         },
         methods: {
         },
         mounted() {
             // Article Api Call
+            axios.get('api/types').then(response => {
+                this.types = response.data.data;
+                console.log(this.types);
+            }).catch(error => {
+                console.log(error);
+            });
             axios.get('api/restaurants').then(response => {
                 this.restaurants = response.data.data;
+                console.log(this.restaurants);
             }).catch(error => {
                 console.log(error);
             });
         },
+        
     }
 </script>
