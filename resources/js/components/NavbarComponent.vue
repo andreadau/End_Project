eeec5622ed7e8b0eaf1335f312bbe430df831dc6
Angela Caterina Gallo <angela.caterina@icloud.com>
2021-03-19 18:30:39 +0100

    fix

--- resources/js/components/NavbarComponent.vue
+++ resources/js/components/NavbarComponent.vue
@@ -1,21 +1,17 @@
 <template>
-<<<<<<< HEAD
-    
-=======
-           <nav>
-                <div class="container_nav">
-                    <div class="logo">
-                        <img src="../../../public/img/giphy.gif" alt="logo"> 
-                        <h2>Delive<span>Boo</span></h2>
-                    </div>
+    <nav>
+        <div class="container_nav">
+            <div class="logo">
+                <img src="../../../public/img/giphy.gif" alt="logo"> 
+                 <h2>Delive<span>Boo</span></h2>
+            </div>
                         
-                    <div class="nav_link">
-                        <a href="/login"> Ristoranti </a>
-                        <a href="/register"> Registrati </a>
-                    </div>
-                </div>
-            </nav>
->>>>>>> 897113477611f3eaed177d2c2be2038bdd52f76e
+            <div class="nav_link">
+                <a href="/login"> Ristoranti </a>
+                <a href="/register"> Registrati </a>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script>
