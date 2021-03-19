915f8db15ebc979362cc8e9829352e63a8d7960a
andreadau <dauriaandrea99@gmail.com>
2021-03-19 15:24:54 +0100

    Change Page Controller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,11 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use App\Restaurant;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function index(){
-        return view('guests.index');
+       /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $restaurants = Restaurant::all();
+        return view('guests.index', compact('restaurants'));
     }
+
+    
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Restaurant  $restaurant
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Restaurant $restaurant)
+    {
+        return view('guests.show', compact('restaurant'));
+    }
+
 }
