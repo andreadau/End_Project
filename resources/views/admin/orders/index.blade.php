aa2748c900520c046defd5e817dd72d3ae8468f0
Angela Caterina Gallo <angela.caterina@icloud.com>
2021-03-29 22:43:00 +0200

    fix

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -1,21 +1,24 @@
 @extends('layouts.admin.dashboard')
 
 @section('content')
-    <h1>All orders</h1>
-    
-    <div class="table_admin">
-        <div class="row_table_th">
-            <div class="title_th">Name</div>
-            <div class="series_th">Price</div>
-            {{-- <div class="status_th"><i class="fas fa-traffic-light"></i></div> --}}
+    <div id="orders">
+        <h1>All orders</h1>
+        
+        <div class="order_show">
+            @foreach($orders as $value)
+                <div class="card_order">
+                    <div class="number_order">{{$value->id}}</div>
+                    <div class="info_customer">
+                        <div>{{$value->customer_name}} {{$value->customer_surname}}</div>
+                        <div>{{$value->customer_address}} {{$value->customer_city}}, {{$value->customer_CAP}} </div>
+                        <div>{{$value->customer_phone}}</div>
+                    </div>
+                    
+                    <div class="total_order">{{$value->total_price}}</div>
+                    {{-- <div class="status {{$value->available ? 'green' : 'red'}}"><i class="fas fa-circle"></i></div> --}}
+                </div>
+            @endforeach
         </div>
-        @foreach($orders as $value)
-            <div class="row_table">
-                <div>{{$value->id}}</div>
-                <div class="name">{{$value->customer_name}}</div>
-                <div class="address">{{$value->total_price}}</div>
-                {{-- <div class="status {{$value->available ? 'green' : 'red'}}"><i class="fas fa-circle"></i></div> --}}
-            </div>
-        @endforeach
     </div>
+    
 @endsection
