0758733d3ea8f8fd0b678be80ec00ad0533d4af1
PierMario Salis <piermario@pop-os.localdomain>
2021-03-30 10:32:27 +0200

    End Backoffice style

--- resources/views/admin/restaurants/show.blade.php
+++ resources/views/admin/restaurants/show.blade.php
@@ -1,29 +1,6 @@
 @extends('layouts.admin.dashboard')
 
 @section('content')
-
-    <!-- <div class="show_restaurant">
-        <h1>
-            {{$restaurant->name}}
-            {{-- <span class="{{$restaurant->available ? 'green' : 'red'}}">
-                <i class="fas fa-circle"></i>
-            </span> --}}
-        </h1>
-        <p>{{$restaurant->city}}</p>
-        <p>{{$restaurant->address}}</p>
-
-        <div class="types">
-            Types: 
-            @if (count($restaurant->types) > 0)
-                @foreach ($restaurant->types as $type)
-                    <span>{{ $type->name }}</span>
-                @endforeach
-            @else
-                <span>N/A</span>
-            @endif
-        </div>
-    </div> -->
-    
     <div class="show_restaurant">
             <div class="card_show_restaurant">
                 <h2>{{$restaurant->name}}
@@ -36,13 +13,13 @@
                 </div>
                 <h3>@if (count($restaurant->types) > 0)
                 @foreach ($restaurant->types as $type)
-                    <span>{{ $type->name }}</span>
+                    <span>Tipologia: {{ $type->name }}</span>
                 @endforeach
             @else
                 <span>N/A</span>
             @endif</h3>
-                <h4>{{$restaurant->city}}</h4>
-                <p>{{$restaurant->address}}</p>
+                <h4>Città: {{$restaurant->city}}</h4>
+                <p> Indirizzo: {{$restaurant->address}}</p>
             </div>          
     </div>
 @endsection
